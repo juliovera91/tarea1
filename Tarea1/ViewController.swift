6499b77049f8c5308ac6a64c7b4cb68faaef87f1
Julio Cesar Vera Segura <JulioVera@macbook-pro-de-julio.local>
2015-11-22 19:57:27 -0600

    Initial Commit

---
+++ Tarea1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tarea1
+//
+//  Created by Julio Cesar Vera Segura on 22/11/15.
+//  Copyright © 2015 Julio Cesar Vera Segura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
